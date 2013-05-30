902e46d76f355a661eb04ad1cfdfe79a7851d0a2
Florian Wolff <florian@donuz.de>
2013-05-30 10:50:04 +0200

    visichart

--- src/VisiChunk.cpp
+++ src/VisiChunk.cpp
@@ -17,6 +17,7 @@
  */
 
 #include <iostream>
+#include <math.h>
 
 #ifdef WIN32
 # include "getopt.h"
@@ -26,7 +27,7 @@
 #endif
 
 #include "lib/config.h"
-#include "lib/Sha1.h"
+#include "lib/FileInputStream.h"
 #include "lib/Exception.h"
 
 using namespace std;
@@ -43,6 +44,8 @@ void showUsage(const char* arg0)
 static bool html = false;
 static bool listUnchangedBlocks = true;
 static int chunkSize = 1024 * 1024;
+static string oldFile;
+static string newFile;
 
 
 void parseCommandlineArgs(int argc, char** argv)
@@ -79,27 +82,122 @@ void parseCommandlineArgs(int argc, char** argv)
   if (argc != optind + 2) {
     cerr << "Wrong number of arguments." << endl;
     showUsage(argv[0]);
-//    while (optind < argc) {
-//      if (operation.empty()) {
-//        // First argument is OPERATION:
-//        operation = argv[optind++];
-//      } else {
-//        cliArgs.push_back(argv[optind++]);
-//      }
-//    }
+    exit(1);
+  } else {
+    oldFile = argv[optind++];
+    newFile = argv[optind++];
   }
-
 }
+
+
+#define CHANGETYPE_CHANGE 1
+#define CHANGETYPE_GROWN 2
+#define CHANGETYPE_SHRUNK 3
+
+void run()
+{
+  FileInputStream oldFileIn(oldFile);
+  FileInputStream newFileIn(newFile);
+  long numSame = 0;
+  long numChange = 0;
+  long numGrown = 0;
+  long numShrunk = 0;
+  long oldtotal = 0;
+  long newtotal = 0;
+  long smetotal = 0;
+  long chgtotal = 0;
+  long blktotal = 0;
+  int numchunks = 0;
+  bool oldend = false;
+  bool newend = false;
+  char* oldbuf = (char*) malloc(chunkSize);
+  char* newbuf = (char*) malloc(chunkSize);
+
+  while (!oldend || !newend) {
+    int type = CHANGETYPE_CHANGE;
+    int oldread, newread;
+
+    if (oldend) {
+      type = CHANGETYPE_GROWN;
+      numGrown++;
+    } else {
+      oldread = oldFileIn.read(oldbuf, chunkSize);
+      //print "old: $oldbuf\n";
+      oldend = (oldread != chunkSize);
+      oldtotal += oldread;
+    }
+
+    if (newend) {
+      type = CHANGETYPE_SHRUNK;
+      numShrunk++;
+    } else {
+      newread = newFileIn.read(newbuf, chunkSize);
+      //print "new: $newbuf\n";
+      newend = (newread != chunkSize);
+      newtotal += newread;
+    }
+
+    if (type == CHANGETYPE_CHANGE) {
+      int cmpsize = oldread < newread ? oldread : newread;
+      blktotal += cmpsize;
+      int cmpdiff = 0;
+      for (int x = 0; x < cmpsize; x++) {
+        if (oldbuf[x] != newbuf[x]) cmpdiff ++;
+      }
+
+      if (cmpdiff == 0) {
+        smetotal += cmpsize;
+        numSame++;
+        // Gleicher Block
+        if (html) {
+          printf ("<div class=\"c e\"></div>");
+        } else {
+          if (listUnchangedBlocks) {
+            printf ("%d\tEQ\n", numchunks);
+          }
+        }
+      } else {
+        chgtotal += cmpdiff;
+        numChange++;
+        // Anderer Block
+        int cl = ceil(cmpdiff * 100/ cmpsize);
+        if (html) {
+          printf ("<div class=\"c c%i\"></div>", cl);
+        } else {
+          printf ("%d\t%d\t%i\n", numchunks, cmpdiff, cl);
+        }
+      }
+    } else if (type == CHANGETYPE_GROWN) {
+      // Grown
+      if (html) {
+        printf ("<div class=\"c g\"></div>");
+      } else {
+        printf ("%d\tGROWN\n", numchunks);
+      }
+    } else if (type == CHANGETYPE_SHRUNK) {
+      // Shrunk
+      if (html) {
+        printf ("<div class=\"c s\"></div>");
+      } else {
+        printf ("%d\tSHRUNK\n", numchunks);
+      }
+    } else {
+      exit(10);
+    }
+
+    numchunks++;
+  }
+};
 
 
 int main(int argc, char** argv)
 {
   try {
     parseCommandlineArgs(argc, argv);
+    run();
   } catch (Exception& ex) {
     cerr << ex.getMessage() << endl;
-//    config.runErrorCallbacks(ex.getMessage());
-    return 10;
+    return 2;
   }
 }
 
